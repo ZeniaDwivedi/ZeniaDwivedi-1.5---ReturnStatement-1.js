ebe6a9bbfbc729ea752a514565c2b4c9eafeb111
ZeniaDwivedi <159184288+ZeniaDwivedi@users.noreply.github.com>
2024-04-22 14:00:44 -0400

    Create 1.5---ReturnStatement-1.js

---
+++ 1.5---ReturnStatement-1.js
@@ -0,0 +1,44 @@
+Is It The Weekend?
+  
+function is_it_the_weekend(day) {
+  if (num == 1,7) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+  Even number divisible by nine
+
+function evNineIble(value) {
+   if (num == %9) { /even/ } {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function evNineIble(value) {
+    if (value % 9 === 0 && value % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+Surface Area of a Cylinder (Medium)
+
+function area_circle(r) {
+    if (num == 1,7) {
+    return true;
+  } else {
+    return false;
+  }
+}
+  
+function area_cylinder(r, h) {
+
+
+
